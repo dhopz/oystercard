6e2d5cdda7ef0b28466b65f3f58db1c1a6af916e
David Houpapa <david_houpapa@hotmail.com>
2021-09-02 17:10:48 +0100

    added fare method for boolean params exit and entry station

--- lib/journey.rb
+++ lib/journey.rb
@@ -3,11 +3,12 @@ class Journey
     attr_reader :fare
     attr_accessor :entry_station, :exit_station
 
-    PENALTY_FARE = 1
+    PENALTY_FARE = 6
 
     def initialize(params)        
         @fare = PENALTY_FARE
         @entry_station = params[:entry_station]  
+        @exit_station = params[:exit_station]  
     end
     
     def complete?
@@ -16,7 +17,12 @@ class Journey
 
     def finish(station)
         @exit_station = station
+        @fare = fare
         self
+    end
+
+    def fare
+        !!@exit_station && !!entry_station ? 1 : 6
     end
 
     def entry_station
